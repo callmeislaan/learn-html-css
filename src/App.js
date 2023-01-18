9beb3b9cf9071d45de00d5f17e500ea43e889b07
phuocht <phuocht@hiworld.com.vn>
2023-01-18 08:53:12 +0700

    lesson3

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='Image'>
+        <FontAwesomeIcon icon={faEnvelope} />
+      </div>
+      <div className='Title'>
+        Get the invite
+      </div>
+      <div className='Content'>
+        Ever wonder what “envelope” might look like in the mix? Well, you're in luck!
+      </div>
+      <form>
+        <table>
+          <tr>
+            <input type='text' className="Input" placeholder='Enter Email' />
+          </tr>
+          <tr>
+            <input type='button' className='Button' value='Request invite' />
+          </tr>
+        </table>
+      </form>
     </div>
   );
 }
