10d731bbdaa11e668bc5d793861281438b3a4fff
phuocht <phuocht@hiworld.com.vn>
2023-01-18 16:05:31 +0700

    lesson6

--- src/App.js
+++ src/App.js
@@ -4,20 +4,21 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2><span>Great example built with bootslander</span></h2>
+      <div className='Slide-group'>
+        <div className='Slide'>
+          <img src={logo} width="200px"/>
+        </div>
+        <div className='Slide'>
+          <img src={logo} width="200px"/>
+        </div>
+        <div className='Slide'>
+          <img src={logo} width="200px"/>
+        </div>
+        <div className='Slide'>
+          <img src={logo} width="200px"/>
+        </div>
+      </div>
     </div>
   );
 }
