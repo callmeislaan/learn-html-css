fe7e720653be7e6fa3840273c8256cfc208eb657
phuocht <phuocht@hiworld.com.vn>
2023-01-20 16:38:47 +0700

    lesson20

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDesktop, faMobile, faWarehouse } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='Title'>
+      <h2>Hello, we have bought together the best quality
+services, offers, projects for you!
+</h2>
+      </div>
+      <div className='Blocks'>
+        <div className='Block'>
+          <FontAwesomeIcon className='Icon' icon={faDesktop}/>
+          <h2>Outstanding design</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sit amet suscipit est.</p>
+        </div>
+        <div className='Block'>
+          <FontAwesomeIcon className='Icon' icon={faMobile}/>
+          <h2>Responsive Layout</h2>
+          <p>Consectetur adipiscing elit. Nulla sit amet suscipit est. Nulla iaculis ipsum sit amet leo euismod maximus.</p>
+        </div>
+        <div className='Block'>
+          <FontAwesomeIcon className='Icon' icon={faWarehouse}/>
+          <h2>Easy to use</h2>
+          <p>Nulla iaculis ipsum sit amet leo euismod maximus. consectetur adipiscing elit. Nulla sit amet.</p>
+        </div>
+
+      </div>
     </div>
   );
 }
