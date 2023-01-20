fc4ad66da987d8b57ae287a103804c4ae1e324c2
phuocht <phuocht@hiworld.com.vn>
2023-01-20 17:28:18 +0700

    lesson21

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMobileScreenButton, faTabletScreenButton } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='Element'>
+        <div className='Left'>
+          <h2>A Elegant Design</h2>
+          <p>Consectetur adipiscing elit. Nulla sit amet suscipit est. Nulla iaculis ipsum sit amet leo euismod maximus. <b>Mauris</b> in ultricies nulla. Quisque vitae ipsum quis odio efficitur tincidunt nec at lectus.</p>
+          <button>BLUE BUTTON WITH MODAL VIDEO</button>
+        </div>
+        <div className='Right Center'>
+          <FontAwesomeIcon className='Icon' icon={faMobileScreenButton} />
+          <FontAwesomeIcon className='Icon' icon={faTabletScreenButton} />
+        </div>
+      </div>
+
+      <div className='Element'>
+        <div className='Left Center'>
+          <FontAwesomeIcon className='Icon' icon={faMobileScreenButton} />
+          <FontAwesomeIcon className='Icon' icon={faTabletScreenButton} />
+        </div>
+        <div className='Right'>
+          <h2>A Elegant Design</h2>
+          <p>Consectetur adipiscing elit. Nulla sit amet suscipit est. Nulla iaculis ipsum sit amet leo euismod maximus. <b>Mauris</b> in ultricies nulla. Quisque vitae ipsum quis odio efficitur tincidunt nec at lectus.</p>
+          <p>Consectetur adipiscing elit. Nulla sit amet suscipit est. Nulla iaculis ipsum sit amet leo euismod maximus. <b>Mauris</b> in ultricies nulla. Quisque vitae ipsum quis odio efficitur tincidunt nec at lectus.</p>
+          <p>Consectetur adipiscing elit. Nulla sit amet suscipit est. Nulla iaculis ipsum sit amet leo euismod maximus. <b>Mauris</b> in ultricies nulla. Quisque vitae ipsum quis odio efficitur tincidunt nec at lectus.</p>
+          <p>Consectetur adipiscing elit. Nulla sit amet suscipit est. Nulla iaculis ipsum sit amet leo euismod maximus. <b>Mauris</b> in ultricies nulla. Quisque vitae ipsum quis odio efficitur tincidunt nec at lectus.</p>
+          <button>BLUE BUTTON WITH MODAL VIDEO</button>
+        </div>
+      </div>
+
+      <div className='Element'>
+        <div className='Right'>
+          <h2>A Elegant Design</h2>
+          <p>Consectetur adipiscing elit. Nulla sit amet suscipit est. Nulla iaculis ipsum sit amet leo euismod maximus. <b>Mauris</b> in ultricies nulla. Quisque vitae ipsum quis odio efficitur tincidunt nec at lectus.</p>
+          <p>Consectetur adipiscing elit. Nulla sit amet suscipit est. Nulla iaculis ipsum sit amet leo euismod maximus. <b>Mauris</b> in ultricies nulla. Quisque vitae ipsum quis odio efficitur tincidunt nec at lectus.</p>
+          <p>Consectetur adipiscing elit. Nulla sit amet suscipit est. Nulla iaculis ipsum sit amet leo euismod maximus. <b>Mauris</b> in ultricies nulla. Quisque vitae ipsum quis odio efficitur tincidunt nec at lectus.</p>
+          <p>Consectetur adipiscing elit. Nulla sit amet suscipit est. Nulla iaculis ipsum sit amet leo euismod maximus. <b>Mauris</b> in ultricies nulla. Quisque vitae ipsum quis odio efficitur tincidunt nec at lectus.</p>
+          <button>BLUE BUTTON WITH MODAL VIDEO</button>
+        </div>
+        <div className='Left Center'>
+          <FontAwesomeIcon className='Icon' icon={faMobileScreenButton} />
+          <FontAwesomeIcon className='Icon' icon={faTabletScreenButton} />
+        </div>
+      </div>
+
     </div>
   );
 }
