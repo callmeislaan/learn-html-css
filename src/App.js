f3c72a2e2c970989c2fb8bf61217268d9513c4cf
phuocht <phuocht@hiworld.com.vn>
2023-01-20 13:47:48 +0700

    lesson18

--- src/App.js
+++ src/App.js
@@ -4,60 +4,83 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <ul>
-        <li>
-        <div className='Component'>
-          <div className='Image'>H</div>
-          <div className='Content-group'>
-            <div className='Title'>
-              CLIMATE CHANGE
-            </div>
-            <div className='Content'>
-              Etiam cras ac sapien ipsum. Morbi scelerisque efficitur nibh a fringilla. Nam et diam vel justo praesent rhoncus volutpat turpis vitae hendrerit.
-            </div>
+      <div className='Top'>
+        <h1>Issues & Plans</h1>
+        <p>
+          Quisque efficitur ipsum ante, eu pretium neque tristique vitae.
+          Donec ac ipsum in lorem molestie.
+        </p>
+      </div>
+
+      <div className='Bottom'>
+        <div className='Left'>
+          <div className='Left-image'>
+            Học Web Chuẩn
+          </div>
+          <div className='Left-content-group'>
+            <h2>"Turkey Will Continues To Make Structure Economic Reforms"</h2>
+            <p>Quisque efficitur ipsum ante, eu pretium neque tristique vitae. Donec ac ipsum in lorem molestie vestibulum ornare eu nibh. Donec ac velit.</p>
+            <p>Praesent commodo nunc eros. Vivamus iaculis eleifend est, non blandit risus interdum id.</p>
           </div>
         </div>
-        </li>
-        <li>
-        <div className='Component'>
-          <div className='Image'>H</div>
-          <div className='Content-group'>
-            <div className='Title'>
-              CLIMATE CHANGE
-            </div>
-            <div className='Content'>
-              Etiam cras ac sapien ipsum. Morbi scelerisque efficitur nibh a fringilla. Nam et diam vel justo praesent rhoncus volutpat turpis vitae hendrerit.
-            </div>
-          </div>
+
+        <div className='Right'>
+          <ul>
+            <li>
+              <div className='Component'>
+                <div className='Image'>H</div>
+                <div className='Content-group'>
+                  <div className='Title'>
+                    CLIMATE CHANGE
+                  </div>
+                  <div className='Content'>
+                    Etiam cras ac sapien ipsum. Morbi scelerisque efficitur nibh a fringilla. Nam et diam vel justo praesent rhoncus volutpat turpis vitae hendrerit.
+                  </div>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div className='Component'>
+                <div className='Image'>H</div>
+                <div className='Content-group'>
+                  <div className='Title'>
+                    CLIMATE CHANGE
+                  </div>
+                  <div className='Content'>
+                    Etiam cras ac sapien ipsum. Morbi scelerisque efficitur nibh a fringilla. Nam et diam vel justo praesent rhoncus volutpat turpis vitae hendrerit.
+                  </div>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div className='Component'>
+                <div className='Image'>H</div>
+                <div className='Content-group'>
+                  <div className='Title'>
+                    CLIMATE CHANGE
+                  </div>
+                  <div className='Content'>
+                    Etiam cras ac sapien ipsum. Morbi scelerisque efficitur nibh a fringilla. Nam et diam vel justo praesent rhoncus volutpat turpis vitae hendrerit.
+                  </div>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div className='Component'>
+                <div className='Image'>H</div>
+                <div className='Content-group'>
+                  <div className='Title'>
+                    CLIMATE CHANGE
+                  </div>
+                  <div className='Content'>
+                    Etiam cras ac sapien ipsum. Morbi scelerisque efficitur nibh a fringilla. Nam et diam vel justo praesent rhoncus volutpat turpis vitae hendrerit.
+                  </div>
+                </div>
+              </div>
+            </li>
+          </ul>
         </div>
-        </li>
-        <li>
-        <div className='Component'>
-          <div className='Image'>H</div>
-          <div className='Content-group'>
-            <div className='Title'>
-              CLIMATE CHANGE
-            </div>
-            <div className='Content'>
-              Etiam cras ac sapien ipsum. Morbi scelerisque efficitur nibh a fringilla. Nam et diam vel justo praesent rhoncus volutpat turpis vitae hendrerit.
-            </div>
-          </div>
-        </div>
-        </li>
-        <li>
-        <div className='Component'>
-          <div className='Image'>H</div>
-          <div className='Content-group'>
-            <div className='Title'>
-              CLIMATE CHANGE
-            </div>
-            <div className='Content'>
-              Etiam cras ac sapien ipsum. Morbi scelerisque efficitur nibh a fringilla. Nam et diam vel justo praesent rhoncus volutpat turpis vitae hendrerit.
-            </div>
-          </div>
-        </div>
-        </li>
-      </ul>
+      </div>
     </div>
   );
 }
