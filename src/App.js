4fab3198f36c806dbe6013250cff2b31907e3cf7
phuocht <phuocht@hiworld.com.vn>
2023-01-20 07:43:38 +0700

    lesson16

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheck } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className='About'>
+        <div className='Image'>
+          Học Web Chuẩn
+        </div>
+        <div className='About-group'>
+          <h1 className='About-us Uppercase'>About Us</h1>
+          <p className='Content'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sed ipsum erat. Donec feugiat ipsum ac risus auctor dignissim. Phasellus ante dolor, molestie vitae malesuada id, posuere non est. Phasellus vulputate pharetra tempor. Aliquam in tortor in arcu iaculis malesuada.</p>
+          <h3 className='Question'>Why Choose us?</h3>
+          <div className='Reason'>
+            <ul>
+              <li><FontAwesomeIcon icon={faCheck}/> Lorem ipsum dolor</li>
+              <li><FontAwesomeIcon icon={faCheck}/> Lorem ipsum dolor</li>
+              <li><FontAwesomeIcon icon={faCheck}/> Lorem ipsum dolor</li>
+              <li><FontAwesomeIcon icon={faCheck}/> Lorem ipsum dolor</li>
+              <li><FontAwesomeIcon icon={faCheck}/> Lorem ipsum dolor</li>
+              <li><FontAwesomeIcon icon={faCheck}/> Lorem ipsum dolor</li>
+              <li><FontAwesomeIcon icon={faCheck}/> Lorem ipsum dolor</li>
+              <li><FontAwesomeIcon icon={faCheck}/> Lorem ipsum dolor</li>
+            </ul>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
