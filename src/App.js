ed06aa47dae6aa3a45318e9740c7287e3edfa6cc
phuocht <phuocht@hiworld.com.vn>
2023-01-20 07:15:46 +0700

    lesson15

--- src/App.js
+++ src/App.js
@@ -4,20 +4,23 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3 className='Title'>Get in touch with us by filling contact form below</h3>
+      <form className='Input-group'>
+        <div>
+          <input className='Input Half' type="input" placeholder='*Enter your full name' />
+          <input className='Input Half' type="email" placeholder='*Enter your email address' />
+        </div>
+        <div>
+          <input className='Full Input' type="input" placeholder="*Enter your subject" />
+        </div>
+        <div>
+          <textarea className='Full Input' placeholder="*Your message here" />
+        </div>
+        <div className='Bottom'>
+          <input type="button" value="Send message" />
+          <span>* Please fill all require form field, thanks!</span>
+        </div>
+      </form>
     </div>
   );
 }
